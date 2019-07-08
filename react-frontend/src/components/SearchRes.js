1c5f6b946155a2eb7b1029e87421413b84407ffa
Gaglani <dgaglani@informatica.com>
2019-07-08 16:52:35 -0400

    Added excel to generate json

--- react-frontend/src/components/SearchRes.js
+++ react-frontend/src/components/SearchRes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 function SearchRes(props) {
-    return (<p>For SearchRes prop: {props.userType}</p>)
+    return (<p>For SearchRes for: {props.userType} with term: {props.searchTerm}</p>)
 }
 
 export default SearchRes
